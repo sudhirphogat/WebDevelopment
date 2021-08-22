2d64dd61506ba7ec4a2bf98f54d91121ae0d336c
sudhirphogat <sudhir.phogat87@gmail.com>
2021-08-22 17:07:23 +0530

    loops & Range- slice,map,string,struct

--- main.go
+++ main.go
@@ -1,35 +1,71 @@
 package main
 
-import "log"
+import (
+	"log"
+)
 
-//decision if & switch case
 func main() {
-	var myVar string
-	myVar = "dog"
-	isTrue := false
-
-	if myVar == "cat" && isTrue {
-		log.Println("The variable is cat and isTrue is true")
-	} else if myVar == "dog" && isTrue == false {
-		log.Println("The var is dog and isTrue is false")
-	} else if myVar == "cat" || isTrue {
-		log.Println("This is an or statement of cat and true")
-	} else {
-		log.Println("nothing is correct")
+	//loops and range
+	//loops
+	for i := 0; i < 7; i++ {
+		if i%2 == 0 {
+			log.Println(i)
+		}
 	}
 
-	myVar2 := "sudhir"
+	//**Range with slice
 
-	switch myVar2 {
-	case "sudhir":
-		log.Println("THis is case 1")
+	animals := []string{"dog", "fish", "Riaan", "horse"}
+	// i will print the index and animal will print one by one range in animal
 
-	case "Sudhir":
-		log.Println("this is case 2")
+	//for i, animal := range animals {
+	// _ is used to keep the variable as blank and ignore so that Go do not throw an error
+	for _, animal := range animals {
+		//log.Println(i, animal)
+		log.Println(animal)
+	}
 
-	default:
-		log.Println("THi is default")
+	//**loop range with Map
 
+	animalsMap := make(map[string]string)
+	animalsMap["dog"] = "charli"
+	animalsMap["horse"] = "pinto"
+
+	for _, animal := range animalsMap {
+		log.Println(animal)
+	}
+
+	//if we want to print the animal type and their name
+	for animalType, animal := range animalsMap {
+		log.Println(animalType, animal)
+	}
+
+	//**loop range through string
+
+	var firstLine = "This is a string"
+	for i, l := range firstLine {
+		log.Println(i, l)
+		//this will not print the letter but the bytes
+
+		log.Printf("%v\t%#U\t", i, l)
+	}
+
+	//**range over customer types
+
+	type User struct {
+		FirstName string
+		LastName  string
+		Email     string
+		Age       int
+	}
+
+	var user []User
+	user = append(user, User{"Sudhir", "Phogat", "sudhir@gmail.com", 30})
+	user = append(user, User{"Ram", "Kumar", "ram@gmail.com", 90})
+	user = append(user, User{"Siva", "Mani", "siva@gmail.com", 69})
+
+	for _, u := range user {
+		log.Println(u.FirstName, u.LastName, u.Email, u.Age)
 	}
 
 }
