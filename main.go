bb22f62c402f2b41dc6a11e01edc2749d125bc08
sudhirphogat <sudhir.phogat87@gmail.com>
2021-08-22 11:39:37 +0530

    type and struct example

--- main.go
+++ main.go
@@ -1,80 +1,41 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"time"
 )
 
-//this variable ouside function' scope is entire package
-var s = "seven"
+//we can define our own type using "type"
+//we  will use struct to group variables
+
+//example
+//below list of variables will increase the list and hectic to remember and maintain
+//******the first letter is CAPS because we can make the variable global to other packages by it
+//********* first small letter variable scope is inside the package only
+//var FirstName string
+//var LastName string
+//var age int
+//var PhoneNumber string
+//var DateOfBirth time.Time
+
+//***use struct to define variable
+type User struct {
+	FirstName   string
+	LastName    string
+	PhoneNumber string
+	Age         int
+	DateOfBirth time.Time
+}
 
 func main() {
+	//struct variable can be difined like this
+	//values that are not used are assigned default values
+	//********the variable user is defined like "**var user User**"
 
-	// we will manipulate some basic functions and features of go lang
+	user := User{
+		FirstName: "Sudhir",
+		LastName:  "Phogat",
+	}
 
-	var somethingNew string
-	var i int
-	// variable declaration
-	somethingNew = "This is a new learning"
-
-	i = 4
-
-	fmt.Println(somethingNew)
-
-	fmt.Println("The value that I want to print is", i)
-
-	// the output can be stored in different variables seperated by comma
-	// how to use both values at different places, ... we will learn in future
-	// := is a new way of declare variable and assign values
-	someNewFunctionString, anotherVariable := aNewFunction()
-
-	fmt.Println("This line is output of function aNewFunction with 2 return values to print", someNewFunctionString, anotherVariable)
-
-	//in below example we will be learning pointer and it is similar to c programming
-	var myPointerString string
-	myPointerString = "Green"
-
-	log.Println("The pointer string is set to ", myPointerString)
-	//when we send value to the fucntion it will show an error
-	//so to send the reference to pointers or memory location to the function we add "&" infront of the string
-	changeUsingPointer(&myPointerString)
-	//check the value of the string after the function.
-	//Note: there is no return in the function but this happens only because the memory value is changed
-	log.Println("the new value of myPointerString after the pointer function is", myPointerString)
-
-	//saysomething function
-
-	sayValue, sayValue2 := saySomething("xxx")
-	log.Println("the output of the saysomething funtion is", sayValue, sayValue2)
-
-}
-
-// it is returning string but it can return more than 1 values by sepearting the types by comma like string, string
-//and putting inside (). for 1 return value () is not required
-func aNewFunction() (string, string) {
-	return "something", "else"
-	//function is returning 2 strings
-
-}
-
-//apply "*" infront of string which means the location
-//so it will not get the value of the value but the memory location
-func changeUsingPointer(s *string) {
-	// the value that s gets is the memory location called pointer
-	log.Println("s is set to ", s)
-	newValue := "Red"
-	//now set the new value in the location by applying * before the string.
-	*s = newValue
-	// Note: -  That we are not returning anything but just changing the value in the memory location
-}
-
-// now the same "s" is a variable in the package scope and we are getting a value of s in function
-// passing value to this function in main function
-func saySomething(s string) (string, string) {
-	//the value of package scope variable is overwritern when passing the value to the function
-	//if the variable name in function is changed like "s to s2"
-	//than value of s will be the value of variable in package scope
-	log.Println("the value of s in saysomething function is", s)
-	return s, "world"
-
+	log.Println(user.FirstName, user.LastName, "Birthday=", user.DateOfBirth)
 }
