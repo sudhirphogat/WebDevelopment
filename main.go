9107489261a6bdb8aae09b269c5a4c12e4bae634
sudhirphogat <sudhir.phogat87@gmail.com>
2021-08-22 13:13:04 +0530

    struct with functions

--- main.go
+++ main.go
@@ -27,6 +27,13 @@ type User struct {
 	DateOfBirth time.Time
 }
 
+//Below variables we will see conversion
+var a int
+
+type checkConvertion int
+
+var b checkConvertion
+
 func main() {
 	//struct variable can be difined like this
 	//values that are not used are assigned default values
@@ -35,7 +42,46 @@ func main() {
 	user := User{
 		FirstName: "Sudhir",
 		LastName:  "Phogat",
+		//end it with a comma
 	}
 
 	log.Println(user.FirstName, user.LastName, "Birthday=", user.DateOfBirth)
+
+	a = 3
+	b = 4
+	log.Println(a)
+	log.Printf("%T\n", a)
+	log.Println(b)
+	log.Printf("%T\n", b)
+
+	// b cannot be assigned to a as a is int and b is checkConversion
+	//a=b
+
+	// ***but it can be converted to another type as the underline of b is int
+	a = int(b)
+	log.Println(a)
+	log.Printf("%T\n", a)
+
+	//**** multiple ways of defining a structure
+
+	var myVar User
+	myVar.FirstName = "Sudhir"
+
+	myVar2 := User{
+		FirstName: "Phogat",
+	}
+
+	//log.Println("The value of myVar is", myVar.FirstName)
+	//log.Println("The value of myVar2 is", myVar2.FirstName)
+
+	//****using function instead of direct variable
+	log.Println("The value of myVar is", myVar.printFirstName())
+	log.Println("The value of myVar2 is", myVar2.printFirstName())
+}
+
+//***Type Struct can also be called using a function by passing  "* struct" before the func name
+//****KeyNote** before function (m *User) where User is type and struct  and after function (s *string) means pointer
+
+func (m *User) printFirstName() string {
+	return m.FirstName
 }
