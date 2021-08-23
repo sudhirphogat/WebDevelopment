2d5a60e213ea904b1c1b85b2d6b08870124151ae
sudhirphogat <sudhir.phogat87@gmail.com>
2021-08-23 12:52:58 +0530

    Channels to create Random number

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,5 +1,10 @@
 package helpers
 
+import (
+	"math/rand"
+	"time"
+)
+
 //I created a folder and in that folder i created this file
 //package name above is folder name
 
@@ -8,4 +13,14 @@ package helpers
 type SomeType struct {
 	TypeName   string
 	TypeNumber int
+}
+
+//function for channel example to create random number
+//function will take n integer and return integer
+func RandomNumber(n int) int {
+
+	//this rand.seed is added to make the random number unique, else always same number was generated
+	rand.Seed(time.Now().UnixNano())
+	value := rand.Intn(n)
+	return value
 }
--- main.go
+++ main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"WebDevelopment/helpers"
 	"fmt"
+	"log"
 )
+
+//the random number will be below numPool
+const numPool = 10
 
 //creating packages
 // in the terminal create a module by running the command "go mod init github.com/<yourid>/<modulename>"
@@ -16,5 +20,39 @@ func main() {
 	myVar.TypeName = "This is taken from another folder"
 
 	fmt.Println(myVar.TypeName)
+
+	//***Channels
+	//*** channels are useful when we have more than many packages inside project
+
+	//we can pass value to fucntions and get return value from them through channels
+	//initialise the channel ** similar to creating MAP
+	intChan := make(chan int)
+
+	//close the channel as a good practise anyhow it is not required.
+	//it is similar to closing a sql connection or file after it is used
+	defer close(intChan)
+
+	//concurrent operation by sending it as routine by using go
+	go CalculateValue(intChan)
+
+	//get the response from the channel after sending from above routine
+	//<- is what is assigned to intchan
+	num := <-intChan
+
+	log.Println(num)
+	//When running the program always the result is same 1.
+	//To fix this we need to make the random number psudo in helper.
+
+}
+
+//creating function for the channal to create random number
+//this will accept a channel value
+//Random value will be created in Helper folder under helpers
+func CalculateValue(intChan chan int) {
+
+	//Function is called from helper and value is passed which is declared in package scope above
+	randomNumber := helpers.RandomNumber(numPool)
+	//pass the random number through channel. where intChan is the channel we created
+	intChan <- randomNumber
 
 }
