5b3c0f2b7f7a39ae90b4698038e37a0ad4014f04
sudhirphogat <sudhir.phogat87@gmail.com>
2021-09-01 17:35:45 +0530

    HTML pages and segregate the code into different files

--- handlers.go
+++ handlers.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
 	"net/http"
 )
 
@@ -14,14 +12,4 @@ func Home(w http.ResponseWriter, r *http.Request) {
 //About is called when /about is called
 func About(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "about.page.html")
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	parseTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parseTemplate.Execute(w, nil)
-
-	if err != nil {
-		fmt.Println("error is parsing template: ", err)
-		return
-	}
 }
---
+++ render.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parseTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parseTemplate.Execute(w, nil)
+
+	if err != nil {
+		fmt.Println("error is parsing template: ", err)
+		return
+	}
+}
